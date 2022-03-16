ad57c5836ed9d59460f9ba4431b45a0b4787285b
Hugo Wood <hugo.wood@zenika.com>
2022-03-16 23:39:26 +0100

    🚧 push json building to sql

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -1,4 +1,5 @@
-drop view if exists article_for_user;
+drop function if exists "/articles";
+drop function if exists articles_for_user;
 drop view if exists article_favorite_count;
 drop table if exists favorite;
 drop table if exists follow;
@@ -46,26 +47,124 @@ create view article_favorite_count as (
   group by article_id
 );
 
-create view article_for_user as (
-  select
-    article.slug,
-    article.title,
-    article.description,
-    article.body,
-    article.created_at,
-    article.updated_at,
-    author.bio as author_bio,
-    author.image as author_image,
-    author.name as author_name,
-    favorite.user_id is not null as favorited,
-    article_favorite_count.count as favorites_count,
-    follow.follower_id
-  from article
-  join "user" as author on author.id = article.author_id
-  left join favorite on favorite.article_id = article.id
-  left join article_favorite_count on article_favorite_count.article_id = article.id
-  left join follow on follow.following_id = author.id
-);
+create function articles_for_user (
+  requesting_user_id bigint,
+  filter_by_author_name text,
+  filter_by_favoriter_name text,
+  "offset" bigint,
+  "limit" bigint
+)
+  returns table (
+    title text,
+    slug text,
+    description text,
+    body text,
+    created_at timestamp,
+    updated_at timestamp,
+    favorites_count bigint,
+    favorited boolean,
+    author_bio text,
+    author_image text,
+    author_name text,
+    author_followed boolean
+  )
+  as $$
+    select
+      article.title,
+      article.slug,
+      article.description,
+      article.body,
+      article.created_at,
+      article.updated_at,
+      coalesce(article_favorite_count.count, 0),
+      favorite.user_id is not null,
+      author.bio,
+      author.image,
+      author.name,
+      follow.follower_id is not null
+    from article
+    join "user" as author on author.id = article.author_id
+    left join article_favorite_count on article.id = article_favorite_count.article_id
+    left join "user" as requesting_user on requesting_user.id = requesting_user_id
+    left join follow on
+      follow.following_id = author.id
+      and follow.follower_id = "requesting_user".id
+    left join favorite on
+      favorite.article_id = article.id
+      and favorite.user_id = "requesting_user".id
+    where
+      (author.name = filter_by_author_name or filter_by_author_name is null)
+      and (
+        (
+          select id
+          from "user"
+          where name = filter_by_favoriter_name
+        ) = any(article_favorite_count.favoriters)
+        or filter_by_favoriter_name is null
+      )
+    order by article.created_at desc
+    offset ("offset")
+    limit ("limit")
+  $$
+  language sql
+  stable;
+
+create function "/articles" (
+  requesting_user_email text,
+  filter_by_author_name text,
+  filter_by_favoriter_name text,
+  "offset" bigint,
+  "limit" bigint
+)
+  returns json
+  as $$
+    select
+      json_build_object(
+        'articles',
+        json_agg(
+          json_build_object(
+            'title',
+            title,
+            'slug',
+            slug,
+            'description',
+            description,
+            'body',
+            body,
+            'createdAt',
+            created_at,
+            'updatedAt',
+            updated_at,
+            'favoritesCount',
+            favorites_count,
+            'favorited',
+            favorited,
+            'author',
+            json_build_object(
+              'bio',
+              author_bio,
+              'image',
+              author_image,
+              'username',
+              author_name,
+              'followed',
+              author_followed
+            )
+          )
+        ),
+        'articlesCount',
+        count(*)
+      )
+    from articles_for_user(
+      (select id from "user" where email = requesting_user_email),
+      filter_by_author_name,
+      filter_by_favoriter_name,
+      "offset",
+      "limit"
+    ) as article
+  $$
+  language sql
+  stable;
 
 delete from article;
 delete from "user";

06562e82565718e11b1b94ee32ae5a97b10629e8
Hugo Wood <hugo.wood@zenika.com>
2022-04-27 22:55:19 +0200

    💡 http_endpoint view

--- src/http_endpoints.sql
+++ src/http_endpoints.sql
@@ -108,3 +108,14 @@ create function get_endpoints() returns table (function_name text, method text, 
       regexp_match(routine_name, '^(GET|POST) (/.*)$') as endpoint_info
     where endpoint_info is not null;
   end;
+
+create view http_endpoint (function_name, method, path) as (
+  select
+    routine_name,
+    endpoint_info[1],
+    endpoint_info[2]
+  from
+    information_schema.routines,
+    regexp_match(routine_name, '^(GET|POST) (/.*)$') as endpoint_info
+  where endpoint_info is not null
+);
